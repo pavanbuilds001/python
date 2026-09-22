430e5f7d6686062a6b9a3998509dd1c58d26241e
pavanbuilds001 <pavankumarmakam.dev@gmail.com>
2026-09-22 13:01:08 +0530

    Add files via upload

---
+++ conditionaloperators.py
@@ -0,0 +1,18 @@
+print("Hello class")
+age=int(input("Enter your age: "))
+if(age<=18):
+    print("eligible for vote")
+    print("eligible for bike ride")
+else:
+    print("not eligible for vote")
+    print("not eligible for bike ride")        
+
+number=int(input("enter a number")) 
+if(number==1):
+    print("MRDU")
+elif(number==2):
+    print("MBU")
+elif(number==3):
+    print("gitam")
+else:
+    print("select another college")
